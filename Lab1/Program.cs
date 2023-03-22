df703895c4fabdb6a72382305f6eefc480c28174
Lei Sun <139188566@qq.com>
2023-03-22 14:12:41 -0500

    Modify Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+StringOperate op1 = new StringOperate((word, num) =>
+{
+    return string.Concat(Enumerable.Repeat(word, num));
+});
+Console.WriteLine(op1("hello", 2));
+
+StringOperate op2 = new StringOperate((word, num) =>
+{
+    return word.Remove(word.Length - num);
+});
+Console.WriteLine(op2("hello", 2));
+
+delegate string StringOperate(string wordArg, int numArg);
