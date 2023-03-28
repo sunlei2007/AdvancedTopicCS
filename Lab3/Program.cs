59f2b36dca1e851bb4c305af027b02149f417ed4
Lei Sun <139188566@qq.com>
2023-03-28 17:17:23 -0500

    M

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,5 +1,6 @@
 using Lab3.Data;
 using Microsoft.EntityFrameworkCore;
+using Lab3.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,26 @@ builder.Services.AddDbContext<DBContext>(options =>
 });
 var app = builder.Build();
 
+app.MapGet("/route", (int id, DBContext db) =>
+{
+    Lab3.Model.Route route = db.Routes.Find(id);
+
+    return Results.Ok(route);
+});
+
+app.MapGet("/stop", (int id, DBContext db) =>
+{
+    Stop stop = db.Stops.Find(id);
+
+    return Results.Ok(stop);
+});
+
+app.MapGet("/stopschedule", (int number, int top, DBContext db) =>
+{
+    StopSchedule stopSchedule = db.StopSchedule.Where(c=>c.Stop.Number==number && c.Route.Number==top).FirstOrDefault();
+
+    return Results.Ok(stopSchedule);
+});
 
 app.Run();
  
