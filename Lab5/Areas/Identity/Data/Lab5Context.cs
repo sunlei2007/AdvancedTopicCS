b5777012c15e9881eda3e86d2173a814b3202a43
Lei Sun <139188566@qq.com>
2023-04-07 10:10:29 -0500

    Create

---
+++ Lab5/Areas/Identity/Data/Lab5Context.cs
@@ -0,0 +1,25 @@
+﻿using Lab5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Lab5.Models;
+
+namespace Lab5.Data;
+
+public class Lab5Context : IdentityDbContext<Lab5User>
+{
+    public Lab5Context(DbContextOptions<Lab5Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<Lab5.Models.BankAccount>? BankAccount { get; set; }
+}
---
+++ Lab5/Controllers/BankAccountsController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab5.Data;
+using Lab5.Models;
+using Lab5.Areas.Identity.Data;
+
+namespace Lab5.Controllers
+{
+    public class BankAccountsController : Controller
+    {
+        private readonly Lab5Context _context;
+
+        public BankAccountsController(Lab5Context context)
+        {
+            _context = context;
+        }
+
+        // GET: BankAccounts
+        public async Task<IActionResult> Index()
+        {
+            Lab5User? user=_context.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+
+              return _context.BankAccount != null ? 
+                          View(await _context.BankAccount.Where(x=>x.UserID==user.Id).ToListAsync()) :
+                          Problem("Entity set 'Lab5Context.BankAccount'  is null.");
+        }
+
+        // GET: BankAccounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.BankAccount == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccount
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankAccount);
+        }
+
+        // GET: BankAccounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BankAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Balance")] BankAccount bankAccount)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Lab5User? user = _context.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+                bankAccount.UserID = user.Id;
+                _context.Add(bankAccount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankAccount);
+        }
+
+        // GET: BankAccounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.BankAccount == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccount.FindAsync(id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // POST: BankAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Balance,UserID")] BankAccount bankAccount)
+        {
+            if (id != bankAccount.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bankAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BankAccountExists(bankAccount.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankAccount);
+        }
+
+        public async Task<IActionResult> EditBalance(int? id)
+        {
+            if (id == null || _context.BankAccount == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccount.FindAsync(id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // POST: BankAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditBalance(int id, [Bind("ID,Balance")] BankAccount bankAccount)
+        {
+            if (id != bankAccount.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bankAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BankAccountExists(bankAccount.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankAccount);
+        }
+
+
+        // GET: BankAccounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.BankAccount == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccount
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankAccount);
+        }
+
+        // POST: BankAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.BankAccount == null)
+            {
+                return Problem("Entity set 'Lab5Context.BankAccount'  is null.");
+            }
+            var bankAccount = await _context.BankAccount.FindAsync(id);
+            if (bankAccount != null)
+            {
+                _context.BankAccount.Remove(bankAccount);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BankAccountExists(int id)
+        {
+          return (_context.BankAccount?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
